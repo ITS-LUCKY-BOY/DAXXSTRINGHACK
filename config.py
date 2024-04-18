3c2f3f175f1c013957066263d3307fc466a072bd
ITS-LUCKY-BOY <152284180+ITS-LUCKY-BOY@users.noreply.github.com>
2024-04-18 07:03:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ class Config:
     API_HASH = "32fa482ffd34ceefec26b7ccd73c4fbb"
     #TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
     TOKEN = os.environ.get("TOKEN", None)
-    MONGO_URL = "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority"
+    MONGO_URL = "mongodb+srv://DxLEGEND143:DxLEGEND143@dxlegend.oztipqk.mongodb.net/?retryWrites=true&w=majority&appName=DxLEGEND"
     START_PIC = "https://te.legra.ph/file/92f53162869ce9701386e.jpg"
     SUDOERS = filters.user(["6458155947"])
